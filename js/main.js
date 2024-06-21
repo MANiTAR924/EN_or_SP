8eb6226a74d5b56cd748ad610486e38b473eb38e
manitar <mac21077919050@gmail.com>
2024-06-21 13:02:05 +0300

    add backend

--- js/main.js
+++ js/main.js
@@ -1,37 +1,20 @@
+const http = require('http');
+const fs = require('fs');
 
-// function number (b1 , q) {
-//     for (let i = 0 ; i < 21 ; i++){
-//         console.log(`${i} - ${}`);
-//     };
-// }
-// number(1/2 , 3)
+let server = http.createServer((req ,res) => {
+    res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'})
 
-const autoBrands = ['BMW', 'Toyota', 'Mersedes', 'Opel', 'Pagani', 'Lotus', 'AudiR7']; 
+    if (req.url == '/')
+        fs.createReadStream('./templates/main/index.html').pipe(res)
+    else if(req.url == '/money')
+        fs.createReadStream('./templates/money/money.html').pipe(res)
 
-const randomNum = [2, 4, 6, 234, 662344, 235235, 3.14, 102028, 1, 32, 14.3423, 14/2];
+});
 
-const firstTwo = autoBrands.slice(0 , 2);
-// console.log(firstTwo);
+const PORT = 3000
+const HOST = 'localhost'
 
-// below
-// const multipleOnTwo = randomNum.map(num => {
-//     return num * 2
-// });
-// console.log(multipleOnTwo);
-
-let multipleOnTwo = []
-for (let i = 0; i < randomNum.length; i++){
-    // multipleOnTwo =+
-};
-// console.log();
-// above
-
-let firstLetter = [];
-for (let i = 0; i < autoBrands.length; i++){
-    firstLetter += autoBrands[i].charAt(0)
-};
-// console.log(firstLetter);
-
-
-
+server.listen(PORT, HOST, () => {
+    console.log(`server working http://${HOST}:${PORT}`)
+})
 
