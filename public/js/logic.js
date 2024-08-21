cede468868d8b4398ea7ba8939173ae972d64888
manitar <mac21077919050@gmail.com>
2024-08-21 11:07:12 +0300

    save

--- public/js/logic.js
+++ public/js/logic.js
@@ -1,5 +1,13 @@
-document.getElementById('blank').addEventListener('submit', );
+document.getElementsByClassName('mainButton'), document.addEventListener("click", newBlank)
 
-function (){
-    
+function newBlank(){
+    const input0 = document.querySelector('input').value
+   
+    const datamuseUrl = 'https://api.datamuse.com/words?ml='
+    const resWord = `${datamuseUrl+input0}`
+
+    console.log(fetch(resWord))
+    const p = document.createElement('p')
+    document.body.appendChild(p)
+    console.log(`input = ${input0}`)
 }
