2c3827defeee3633530d057623d8f2bd450c4861
manitar <mac21077919050@gmail.com>
2024-08-27 11:46:09 +0300

    save

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ const fs = require('fs');
 const http = require('http');
 const path = require('path');
 const mimeTypes = {
-    '.html': 'text/html',
     '.js': 'text/javascript',
+    '.html': 'text/html',
     '.css': 'text/css',
     '.json': 'application/json',
     '.png': 'image/png',
@@ -18,40 +18,31 @@ const mimeTypes = {
     '.otf': 'application/font-otf',
     '.wasm': 'application/wasm',
 };
-
-function reading (ext, filePath, req,res) {
-    fs.createReadStream(`${filePath}`).pipe(res);
-    console.log(`${}`)
-
-    for (const i in mimeTypes) {
-        if (i === ext){
-            res.writeHead(200, { 'Content-Type': `${mimeTypes[i]}` });
-            
-        }
-        console.log(mimeTypes[i])
-    }
+async function reading (ext, filePath, req ,res) {
+    console.log(`[ P ] req for ${req.url}`);
+    
+    if (mimeTypes[ext] !== undefined){
+        await fs.createReadStream(`./public${filePath}`).pipe(res);
+        
+        res.writeHead(200, { 'Content-Type': `${mimeTypes[ext]}` });
+        console.log(` [ S ] Success request for ${req.url}`);
+    }    
+    
 };
 const server = http.createServer((req, res) => {
-    console.log(`Request for ${req.url} received.\n`);
+    switch (req.url) {
+        case '/':
+            reading('.html', '/html/home.html', req, res);
+            reading('.css', '/css/main.css', req, res);
+            reading('.js', '/js/logic.js', req, res);
+        case '':
+        default:
+            reading('.html', '/html/error.html', req, req)
+    }
 
-    reading('.html', './public/html/error.html', req, res)
 
-if (req.url === '/') {
-        // reading('.html', './public/html/error.html', req, res)
-        
-}
-//  else if (req.url === '/main.css') {
-//     fs.createReadStream('./public/css/main.css').pipe(res);
-    
-//     res.writeHead(200, { 'Content-Type': 'text/css' });
-// } else if (req.url === '/js/logic.js') {
-//     fs.createReadStream('./public/js/logic.js').pipe(res);
-    
-//     res.writeHead(200, { 'Content-Type': 'text/javascript' });
-// }  
+
 }).listen(3000, () => {
-console.log('Server working on http://localhost:3000');
+console.log('\nServer working on http://localhost:3000\n');
 });
-// console.log(http.request())
-
 
