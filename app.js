a1c435c80cb28c2050c7f46bd13b94c581cca3cc
manitar <mac21077919050@gmail.com>
2024-08-31 12:17:38 +0300

    i add full and good code for reading file, and guess what?) its AUTOMATICA

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const fs = require('fs');
 const http = require('http');
-const path = require('path');
 const mimeTypes = {
     '.js': 'application/javascript',
     '.html': 'text/html',
@@ -19,46 +18,58 @@ const mimeTypes = {
     '.wasm': 'application/wasm',
 };
 
+function fExt (param) {
+    return param.split('.')[param.split('.').length-1];    
+};
 
-async function reading (filePath, req ,res) {
-    const ext = filePath.split('.')[filePath.split('.').length-1];
+function preReading(req,res) {
+    console.log(`[ P ] req (${fExt(req.url)}) for ${req.url}`);
 
-    console.log(`[ P ] req (${ext}) for ${req.url}`);
+    if (req.url === '/'){
+        fs.createReadStream(`./public/html/home.html`).pipe(res);    
+
+        res.writeHead(200, { 'Content-Type': `${mimeTypes[`.html`]}` });
+    } else if (req.url === '/money'){
+        fs.createReadStream(`./public/html/money.html`).pipe(res);    
+
+        res.writeHead(200, { 'Content-Type': `${mimeTypes[`.html`]}` });
+    }else if (fExt(req.url) === 'js'){
+        fs.createReadStream(`./public${req.url}`).pipe(res);    
+
+        res.writeHead(200, { 'Content-Type': `${mimeTypes[`.html`]}` });
+    }else {
+        fs.createReadStream(`./public/${fExt(req.url)}${req.url}`).pipe(res);    
     
-    fs.createReadStream(`./public${filePath}`).pipe(res);
+        res.writeHead(200, { 'Content-Type': `${mimeTypes[`.${fExt(req.url)}`]}` });
+    }
+ console.log(`.${fExt(req.url)}`)
+}
+
+function reading (filePath, req ,res ) {
+
+    console.log(`[ P ] req () for ${req.url}`);
+    
+    fs.createReadStream(`./public/${ext}${req.url}`).pipe(res);    
+    
+    // if (req.url === '/'){
+    //     fs.createReadStream(`./public/${ext}${req.url}`).pipe(res);
+
+    // } else if (req.url === '/money'){
+    //     fs.createReadStream(`./public/${ext}${req.url}`).pipe(res);
+
+    // }
 
     if (mimeTypes[ext] !== undefined){
         
         res.writeHead(200, { 'Content-Type': `${mimeTypes[`.${ext}`]}` });
         // console.log(` [ S ] Success request for ${req.url}`);
     }    
-    return ext
 };
-const server = http.createServer((req, res) => {
-    const s = req.url;
-    // switch (req.url) {
-    //     case '/':
-    //         reading('/html/home.html', req, res);
-    //         // reading('/css/main.css', req, res);
-    //         // reading('/js/logic.js', req, res);
-    //     case '/logic.js':
-    //         reading('/js/logic.js', req, res);
-    //     case '/main.css':
-    //         reading('/css/main.css', req, res);
-    //     default:
-    //         reading('/html/error.html', req, req)
-    // }
-    if (s === '/') {
-        reading('/html/home.html', req , res)
-    } else if (req.url === '/main.css'){
-        reading('/css/main.css', req , res)
-    } else if (req.url === '/js/logic.js'){
-        reading('/js/logic.js', req , res)
-    }
-    
 
+const server = http.createServer((req, res) => {
+
+    preReading(req,res)
 
 }).listen(3000, () => {
 console.log('\nServer working on http://localhost:3000\n');
 });
-
