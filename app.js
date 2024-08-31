5863fd8f016be5ca149ae03a24cf084430546941
manitar <mac21077919050@gmail.com>
2024-08-31 16:40:40 +0300

    save0

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ const mimeTypes = {
     '.eot': 'application/vnd.ms-fontobject',
     '.otf': 'application/font-otf',
     '.wasm': 'application/wasm',
+    '.ico': 'image/x-icon',
 };
 
 function fExt (param) {
@@ -24,47 +25,35 @@ function fExt (param) {
 
 function preReading(req,res) {
     console.log(`[ P ] req (${fExt(req.url)}) for ${req.url}`);
-
-    if (req.url === '/'){
-        fs.createReadStream(`./public/html/home.html`).pipe(res);    
-
-        res.writeHead(200, { 'Content-Type': `${mimeTypes[`.html`]}` });
-    } else if (req.url === '/money'){
-        fs.createReadStream(`./public/html/money.html`).pipe(res);    
-
-        res.writeHead(200, { 'Content-Type': `${mimeTypes[`.html`]}` });
-    }else if (fExt(req.url) === 'js'){
-        fs.createReadStream(`./public${req.url}`).pipe(res);    
-
-        res.writeHead(200, { 'Content-Type': `${mimeTypes[`.html`]}` });
-    }else {
-        fs.createReadStream(`./public/${fExt(req.url)}${req.url}`).pipe(res);    
+    try {
+        if (req.url === '/'){
+            fs.createReadStream(`./public/html/home.html`).pipe(res);    
     
-        res.writeHead(200, { 'Content-Type': `${mimeTypes[`.${fExt(req.url)}`]}` });
-    }
- console.log(`.${fExt(req.url)}`)
-}
-
-function reading (filePath, req ,res ) {
-
-    console.log(`[ P ] req () for ${req.url}`);
+            res.writeHead(200, { 'Content-Type': `${mimeTypes[`.html`]}` });
+        } else if (req.url === '/money'){
+            fs.createReadStream(`./public/html/money.html`).pipe(res);    
     
-    fs.createReadStream(`./public/${ext}${req.url}`).pipe(res);    
+            res.writeHead(200, { 'Content-Type': `${mimeTypes[`.html`]}` });
+        }else if (fExt(req.url) === 'js'){
+            fs.createReadStream(`./public${req.url}`).pipe(res);    
     
-    // if (req.url === '/'){
-    //     fs.createReadStream(`./public/${ext}${req.url}`).pipe(res);
-
-    // } else if (req.url === '/money'){
-    //     fs.createReadStream(`./public/${ext}${req.url}`).pipe(res);
-
-    // }
-
-    if (mimeTypes[ext] !== undefined){
+            res.writeHead(200, { 'Content-Type': `${mimeTypes[`.html`]}` });
+        }else if(req.url === '/favicon.ico'){
+            fs.createReadStream(`./public/jpg/Untitled.jpeg`).pipe(res);    
+    
+            res.writeHead(200, { 'Content-Type': `${mimeTypes[`.iso`]}` });
+        }else {
+            fs.createReadStream(`./public/${fExt(req.url)}${req.url}`).pipe(res);    
         
-        res.writeHead(200, { 'Content-Type': `${mimeTypes[`.${ext}`]}` });
-        // console.log(` [ S ] Success request for ${req.url}`);
-    }    
-};
+            res.writeHead(200, { 'Content-Type': `${mimeTypes[`.${fExt(req.url)}`]}` });
+        };
+    } catch (error) {
+        throw error
+    };
+
+    
+//  console.log(`.${fExt(req.url)}`);
+}
 
 const server = http.createServer((req, res) => {
 
